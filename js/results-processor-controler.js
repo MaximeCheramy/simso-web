df5e2fee8c4b411873ba149ba5f537153d555d23
Josué A <josue.alvarez@hotmail.fr>
2015-06-25 11:39:59 +0000

    Added missing files.

---
+++ js/results-processor-controler.js
@@ -0,0 +1,46 @@
+
+simsoControllers.controller('ResultsProcessorControler', 
+['$scope', '$controller', '$timeout', 'uiGridConstants',
+function($scope, $controller, $timeout, uiGridConstants)
+{
+	// $controller('resultsCtrl', {$scope:$scope});	
+	
+	$scope.gridOptions = {
+		enableRowSelection: false,
+		enableColumnResize: false,
+		enableCellEdit: false,
+		enableColumnMenus: true,
+		enableHorizontalScrollbar: 0,
+		enableVerticalScrollbar: 1,
+		columnDefs: [
+			{name: 'CPU', type: 'string'},
+			{name: 'Ctx Save Count', type: 'number'},
+			{name: 'Ctx Load Count', type: 'number'},
+			{name: 'Ctx Save Overhead', type:'string'},
+			{name: 'Ctx Load Overhead', type:'string'}],
+		minRowsToShow: 5,
+		data: $scope.python["results-processors"]
+	};
+	
+	$scope.gridOptions.onRegisterApi = function(gridApi) {
+		$scope.gridApi = gridApi;
+	};
+	
+	$scope.$watch("conf.window.startDate", function (newValue, oldValue) {
+		$timeout(function() {
+			// Delay changes (dont execute while the user is typing)
+			if($scope.conf.window.startDate == newValue) {
+				$scope.gridOptions.data = $scope.python["results-processors"];
+			}
+		}, 100);
+	});
+	
+	$scope.$watch("conf.window.endDate", function (newValue, oldValue) {
+		$timeout(function() {
+			// Delay changes (dont execute while the user is typing)
+			if($scope.conf.window.endDate == newValue) {
+				$scope.gridOptions.data = $scope.python["results-processors"];
+			}
+		}, 100);
+	});
+}]);
