e00ecd1b149acf2c71171709fb8b59ea7682f722
Josué A <josue.alvarez@hotmail.fr>
2015-07-01 10:28:00 +0000

    Put the scheduler controler in a separate file

--- js/config-controlers.js
+++ js/config-controlers.js
@@ -104,14 +104,3 @@ simsoControllers.controller('ConfigGeneralCtrl', ['confService', '$scope', funct
 	$scope.conf = confService;
 }]);
 
-simsoControllers.controller('ConfigSchedulerCtrl', 
-['confService', 'pypyService',  '$scope', 
-function(confService, pypyService, $scope) {
-	$scope.conf = confService;
-	
-	pypyService.registerObserverCallback($scope, function() {
-		confService.scheduler_list = python["schedulers"];
-		confService.scheduler_class = confService.scheduler_list[0];
-		console.log("EXEC : " + confService.scheduler_list.toSource());
-	});
-}]);
---
+++ js/config-scheduler-controler.js
@@ -0,0 +1,11 @@
+simsoControllers.controller('ConfigSchedulerCtrl', 
+['confService', 'pypyService',  '$scope', 
+function(confService, pypyService, $scope) {
+	$scope.conf = confService;
+	
+	pypyService.registerObserverCallback($scope, function() {
+		confService.scheduler_list = python["schedulers"];
+		confService.scheduler_class = confService.scheduler_list[0];
+		console.log("EXEC : " + confService.scheduler_list.toSource());
+	});
+}]);
